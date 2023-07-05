8a0d9c57093f2ba4808b057f6179d03ef058a58c
NSALAA9 <alaansairat99@gmail.com>
2023-07-05 07:17:38 +0000

    lab 6

--- src/auth/router.js
+++ src/auth/router.js
@@ -1,16 +1,25 @@
 const express = require('express');
-const Users = require('./models/users-model');
-
+const basicAuthMiddleware = require('./middleware/basic');
+const bcrypt = require('bcrypt');
 const router = express.Router();
 
-router.post('/signup', async (req, res, next) => {
-  try {
-    const { username, password } = req.body;
-    const user = await Users.create({ username, password });
-    res.status(201).json({ user });
-  } catch (error) {
-    next(error);
-  }
-});
+router.post('/signup', userSignUp);
+router.post('/signin', basicAuthMiddleware, userSignIn);
+
+async function userSignUp(req, res) {
+  const { userName, password } = req.body;
+  const hashedPassword = await bcrypt.hash(password, 5);
+  const newUser = await User.create({
+    userName: userName,
+    password: hashedPassword
+  });
+  res.status(201).json(newUser);
+}
+
+function userSignIn(req, res) {
+  res.status(200).json({
+    user: req.user
+  });
+}
 
 module.exports = router;
